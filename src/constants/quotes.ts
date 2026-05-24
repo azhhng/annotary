6514c00085de87f6ee8827250b8798e5d6cf83af
azhhng <azhhng@gmail.com>
2026-05-24 17:33:30 -0400

    add more quotes

--- src/constants/quotes.ts
+++ src/constants/quotes.ts
@@ -12,6 +12,22 @@ export const emptyOthersQuotes: EmptyStateQuote[] = [
     text: '"People generally see what they look for, and hear what they listen for."',
     attribution: "- Harper Lee, To Kill a Mockingbird",
   },
+  {
+    text: '"My feelings will not be repressed. You must allow me to tell you how ardently I admire and love you."',
+    attribution: "- Jane Austen, Pride And Prejudice",
+  },
+  {
+    text: '"I know not all that may be coming, but be it what it will, I’ll go to it laughing."',
+    attribution: "- Herman Melville, Moby Dick",
+  },
+  {
+    text: '"Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth."',
+    attribution: "- Marcus Aurelius, Meditations",
+  },
+  {
+    text: '"There is no greater agony than bearing an untold story inside you."',
+    attribution: "- Maya Angelou, I Know Why the Caged Bird Sings",
+  },
 ];
 
 export const emptyEssenceQuotes: EmptyStateQuote[] = [
@@ -24,7 +40,31 @@ export const emptyEssenceQuotes: EmptyStateQuote[] = [
     attribution: "- Sylvia Plath, The Bell Jar",
   },
   {
-    text: "It’s no use going back to yesterday, because I was a different person then.",
+    text: '"It’s no use going back to yesterday, because I was a different person then."',
     attribution: "- Lewis Carroll, Alice in Wonderland",
+  },
+  {
+    text: '"Perhaps one did not want to be loved so much as to be understood."',
+    attribution: "- George Orwell, 1984",
+  },
+  {
+    text: '"Rather than love, than money, than fame, give me truth."',
+    attribution: "- Henry David Thoreau, Walden",
+  },
+  {
+    text: '"When you suffer, I suffer with you. To the end I am close to you."',
+    attribution: "- Shūsaku Endō, Silence",
+  },
+  {
+    text: '"There are too many things we do not wish to know about ourselves."',
+    attribution: "- James Baldwin, The Fire Next Time",
+  },
+  {
+    text: '"He can’t value you more than you value yourself."',
+    attribution: "- Toni Morrison, Song of Solomon",
+  },
+  {
+    text: '"Nobody even imagines how well one can lie about the state of one’s own heart."',
+    attribution: "- Yukio Mishima, Thirst for Love",
   },
 ];
