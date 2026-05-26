4a8641e7f883851456b593a95dce3a282c2d1dd5
azhhng <azhhng@gmail.com>
2026-05-26 00:53:24 -0400

    add more quotes

--- src/constants/quotes.ts
+++ src/constants/quotes.ts
@@ -13,12 +13,36 @@ export const emptyOthersQuotes: EmptyStateQuote[] = [
     attribution: "- Jane Austen, Pride And Prejudice",
   },
   {
-    text: '"I know not all that may be coming, but be it what it will, I’ll go to it laughing."',
-    attribution: "- Herman Melville, Moby Dick",
-  },
-  {
     text: '"Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth."',
     attribution: "- Marcus Aurelius, Meditations",
+  },
+  {
+    text: '"Be kind, aim for my heart."',
+    attribution: "- Alexandre Dumas, The Three Musketeers",
+  },
+  {
+    text: '"We can know only that we know nothing. And that is the highest degree of human wisdom."',
+    attribution: "- Leo Tolstoy, War and Peace",
+  },
+  {
+    text: '"Is it really possible to tell someone else what one feels?"',
+    attribution: "- Leo Tolstoy, Anna Karenina",
+  },
+  {
+    text: '"The world was to me a secret which I desired to devine."',
+    attribution: "- Mary Shelley, Frankenstein",
+  },
+  {
+    text: '"The real loneliness is living among all these kind people who only ask one to pretend!"',
+    attribution: "- Edith Wharton, The Age of Innocence",
+  },
+  {
+    text: '"The world was to me a secret which I desired to devine."',
+    attribution: "- Mary Shelley, Frankenstein",
+  },
+  {
+    text: '"How easy it is to judge rightly after one sees what evil comes from judging wrongly."',
+    attribution: "- Elizabeth Gaskell, Wives and Daughters",
   },
 ];
 
@@ -34,5 +58,17 @@ export const emptyEssenceQuotes: EmptyStateQuote[] = [
   {
     text: '"Rather than love, than money, than fame, give me truth."',
     attribution: "- Henry David Thoreau, Walden",
+  },
+  {
+    text: '"I could have been a great many things."',
+    attribution: "- Louisa May Alcott, Little Women",
+  },
+  {
+    text: '"I’ll not listen to reason... reason always means what someone else has got to say."',
+    attribution: "- Elizabeth Gaskell, Cranford",
+  },
+  {
+    text: '"I know not all that may be coming, but be it what it will, I’ll go to it laughing."',
+    attribution: "- Herman Melville, Moby Dick",
   },
 ];
